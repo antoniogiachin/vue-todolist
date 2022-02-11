b49b9e30410211ef9154a61fc1b872bd6418a9b4
antoniogiachin <giachinantonio@outlook.com>
2022-02-11 17:55:53 +0100

    fine esercizio con bonus, inserisco commenti

--- main.js
+++ main.js
@@ -8,43 +8,39 @@ const app = new Vue (
             logo : "https://yt3.ggpht.com/ytc/AKedOLTO2XVhtyMr24Dnz6QJ-Lsj_05XHim-qMoF6PRc=s900-c-k-c0x00ffffff-no-rj",
 
             todos: [
-                {
-                    text: 'Fai la spesa',
+                
+                // {
+                //     text: 'Manda una email',
 
-                    done : true,
-
-                },
-
-                {
-                    text: 'Manda una email',
-
-                    done: true,
-                },
+                //     done: true,
+                // },
             ],
 
+            newText: '',
+            
             newToDo: {
-                text: '',
 
-                done: true,
+                text: '',                
+
+                done : false,
+
             },
 
-            newText: '',
+            class: '',
 
         },
 
         methods : {
 
+
             addNew(){
 
-                if(this.newText == ''){
-                    alert('Hai inserito un campo vuoto!')
-                } else{
-
-                    // this.newToDo.done=true;
-                    this.newToDo.text= this.newText;
-                    this.todos.push(this.newToDo);
-                    this.newText ='';
-                }
+               this.todos.push({
+                   text: this.newText,
+                   done: false,
+                }) 
+                
+                this.newText ='';
 
             },
 
@@ -53,6 +49,10 @@ const app = new Vue (
                 this.todos.splice(index, 1);
 
             },
+
+            check(index){
+                this.todos[index].done = !this.todos[index].done
+            }
 
         },
 
