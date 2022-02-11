618f54d32810ae00853b14be223b7433f8a8e92a
antoniogiachin <giachinantonio@outlook.com>
2022-02-11 15:34:50 +0100

    stilizzazione completa

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = new Vue (
         el: '#app',
 
         data: {
-
+            logo : "https://yt3.ggpht.com/ytc/AKedOLTO2XVhtyMr24Dnz6QJ-Lsj_05XHim-qMoF6PRc=s900-c-k-c0x00ffffff-no-rj"
         },
 
         methods : {
