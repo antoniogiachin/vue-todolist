bb12b303d28316d173a034142cdc5468d4f0985a
antoniogiachin <giachinantonio@outlook.com>
2022-02-11 15:56:27 +0100

    logica inserimento todo completata

--- main.js
+++ main.js
@@ -5,11 +5,44 @@ const app = new Vue (
         el: '#app',
 
         data: {
-            logo : "https://yt3.ggpht.com/ytc/AKedOLTO2XVhtyMr24Dnz6QJ-Lsj_05XHim-qMoF6PRc=s900-c-k-c0x00ffffff-no-rj"
+            logo : "https://yt3.ggpht.com/ytc/AKedOLTO2XVhtyMr24Dnz6QJ-Lsj_05XHim-qMoF6PRc=s900-c-k-c0x00ffffff-no-rj",
+
+            todos: [
+                {
+                    text: 'Fai la spesa',
+
+                    done : true,
+
+                },
+
+                {
+                    text: 'Manda una email',
+
+                    done: true,
+                },
+            ],
+
+            newToDo: {
+                text: '',
+
+                done: true,
+            },
+
         },
 
         methods : {
 
+            addNewToDo(){
+
+                this.newToDo = '';
+
+                if(this.newToDo == ''){
+                    alert('Hai laciato il campo vuoto!');
+                } else{
+                    this.todos.push(this.newToDo);
+                    this.newToDo = '';
+                }
+            },
 
         },
 
